7e303eaa49cda8151a7a784c652521115da5bf85
dingus4882 <dingus4882@gmail.com>
2026-05-11 12:38:07 -0700

    stuff

---
+++ scenes/enemy.tscn.gd
@@ -0,0 +1,17 @@
+extends Area2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	print("ooga booga")
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+
+func _on_body_entered(body):
+	print("ooh scary")
+	$Label.show()
+	$Label.set_text("Fight(F)")
+
+func _on_body_exited(body):
+	print("oof")
+	$Label.hide()
