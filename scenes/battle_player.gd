423a5fa746b780591893375f879324c53f987455
dingus4882 <dingus4882@gmail.com>
2026-05-10 21:36:01 -0700

    Added extra character sprites and the battle scene setup

---
+++ scenes/battle_player.gd
@@ -0,0 +1,13 @@
+extends Area2D
+var heavyIjured = load("res://assets/heavyinjury.png")
+var injured = load("res://assets/injury.png")
+
+var health = 100
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
