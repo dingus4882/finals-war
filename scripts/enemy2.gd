58df32b7c21f580db0bf81d8cc61e6d58ad0a9a0
dingus4882 <dingus4882@gmail.com>
2026-05-12 16:20:42 -0700

    Finally managed to change the scene to the battle scene. still have to make the actual battle....

--- scripts/enemy2.gd
+++ scripts/enemy2.gd
@@ -1,6 +1,6 @@
 extends Area2D
 
-
+var camZoom = Vector2 (2.5,2.5)
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	print("ooga booga")
@@ -15,3 +15,12 @@ func _on_body_entered(body):
 func _on_body_exited(body):
 	print("oof")
 	$Label.hide()
+
+func _input(event):
+	if event.is_action_pressed("Battle"):
+		#$"../../battle".show()
+		$"../../battleMap".show()
+		$"../../Player".hide()
+		$"../../Player/Camera2D".set_zoom(camZoom)
+		$"..".queue_free()
+		
--- scripts/game.gd
+++ scripts/game.gd
@@ -4,9 +4,8 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$classroomMap.hide()
-	$battle.hide()
-	
-
+	#$battle.hide()
+	$battleMap.hide()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
